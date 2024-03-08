f47c8d9f6ea1ed4e6fcd1eebfe30554fef14f7b8
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-03-08 01:48:54 -0500

    Clean labelme_json_dataset_parsing script

--- scripts/labelme_json_dataset_parsing.py
+++ scripts/labelme_json_dataset_parsing.py
@@ -1,17 +1,13 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-import argparse
-import base64
+from dataclasses import dataclass
 import json
-import os
 from typing import Dict, List, Tuple
 from natsort import natsorted
 import os.path as osp
-
 import PIL.Image
 import numpy as np
 from tqdm import tqdm
-import yaml
 from pathlib import Path
 from labelme.logger import logger
 from labelme import utils
@@ -42,21 +38,17 @@ Labelme json file format:
 """
 
 
+@dataclass
 class LabelMeJsonParser:
     """Class to process a single label me json file"""
 
-    def __init__(
-        self,
-        json_file: Path,
-        label_parser: SegmentationLabelParser = None,
-        label_saver: LabelMeImageSaver = None,
-    ):
+    json_file: Path
+    custom_data_saver: LabelMeImageSaver
+    label_parser: SegmentationLabelParser = None
 
-        self.label_parser: SegmentationLabelParser = label_parser
-        self.label_saver: LabelMeImageSaver = label_saver
+    def __post_init__(self):
 
-        self.json_file: Path = json_file
-        self.data: Dict = json.load(open(json_file))
+        self.data: Dict = json.load(open(self.json_file))
 
         self.label_name_to_value: Dict[str, int] = {"_background_": 0}
         self.label_names: List[str]
@@ -114,8 +106,8 @@ class LabelMeJsonParser:
 
         return lbl, lbl_viz
 
-    def save_images_and_labels(self, out_dir: Path):
-        out_dir.mkdir(exist_ok=True)
+    def save_without_label_parser(self, out_dir: Path):
+        out_dir.mkdir(exist_ok=True, parents=True)
 
         PIL.Image.fromarray(self.img).save(out_dir / "img.png")
         utils.lblsave(out_dir / "label.png", self.lbl)
@@ -128,7 +120,10 @@ class LabelMeJsonParser:
         logger.info("Saved to: {}".format(out_dir))
 
     def save(self):
-        self.label_saver.save_image_and_label(self.img, self.lbl, self.lbl_viz)
+        if self.custom_data_saver is None:
+            raise Exception("Custom data saver was not provided")
+
+        self.custom_data_saver.save_image_and_label(self.img, self.lbl, self.lbl_viz)
 
 
 class LabelMeImageSaver(ABC):
@@ -137,7 +132,9 @@ class LabelMeImageSaver(ABC):
         self.root_path = root_path
 
     @abstractmethod
-    def save_image_and_label(self, img: np.ndarray, lbl: np.ndarray, lbl_viz: np.ndarray) -> None:
+    def save_image_and_label(
+        self, img: np.ndarray, lbl: np.ndarray, lbl_viz: np.ndarray
+    ) -> None:
         """Abstract method to save an image and its label to a directory.
 
         Parameters
@@ -152,10 +149,8 @@ class LabelMeImageSaver(ABC):
         pass
 
 
-class SaveInSingleFolder(LabelMeImageSaver):
-    """Save all images and labels from multiple json files in a single folder. Each image gets the
-    labels of the corresponding json file.
-    """
+class MultiDirSaver(LabelMeImageSaver):
+    """Save raw images and labels in different folders."""
 
     def __init__(self, label_parser, root_path, file_prefix: str):
         super().__init__(label_parser, root_path)
@@ -169,21 +164,29 @@ class SaveInSingleFolder(LabelMeImageSaver):
         self.labels_dir.mkdir(exist_ok=True)
         self.blended_dir.mkdir(exist_ok=True)
 
-    def save_image_and_label(self, img: np.ndarray, lbl: np.ndarray, lbl_viz: np.ndarray):
+    def save_image_and_label(
+        self, img: np.ndarray, lbl: np.ndarray, lbl_viz: np.ndarray
+    ):
         PIL.Image.fromarray(img).save(self.raw_dir / (self.file_prefix + "_img.png"))
         # utils.lblsave(self.labels_dir / (self.file_prefix + "_label.png"), lbl)
         rgb_lbl = self.label_parser.convert_single_ch_to_rgb(lbl)
-        PIL.Image.fromarray(rgb_lbl).save(self.labels_dir / (self.file_prefix + "_label.png"))
-        PIL.Image.fromarray(lbl_viz).save(self.blended_dir / (self.file_prefix + "_label_viz.png"))
+        PIL.Image.fromarray(rgb_lbl).save(
+            self.labels_dir / (self.file_prefix + "_label.png")
+        )
+        PIL.Image.fromarray(lbl_viz).save(
+            self.blended_dir / (self.file_prefix + "_label_viz.png")
+        )
 
 
-class SaveInMultipleFolders(LabelMeImageSaver):
-    """Save the images for each json file in a separate folder."""
+class SingleDirSaver(LabelMeImageSaver):
+    """
+    Save images and labels in same folder
+    """
 
     def save_image_and_label(
         self, img: np.np.ndarray, lbl: np.ndarray, lbl_viz: np.ndarray
     ) -> None:
-        self.root_path.mkdir(exist_ok=True)
+        self.root_path.mkdir(exist_ok=True, parents=True)
 
         PIL.Image.fromarray(img).save(self.root_path / "img.png")
         # utils.lblsave(self.root_path / "label.png", lbl)
@@ -204,63 +207,69 @@ def mycommands():
 
 
 @click.command()
-@click.argument("indir", type=Path)
 @click.option(
-    "--labels_yaml", type=Path, required=True, help="Path to yaml file with class names and ids"
+    "--indir", type=Path, required=True, help="Path to the folder with json files"
 )
-def parse_folder_with_json_files(indir: Path, labels_yaml: Path):
-    """Parse a dataset with multiple labelme json files.
-    Indir should contain a directory json_annotations with the json files.
-    """
+@click.option("--outdir", type=Path, required=True)
+@click.option(
+    "--labels_yaml_path",
+    type=Path,
+    help="Path to yaml file with class names and ids",
+    required=True,
+)
+def parse_folder(indir: Path,outdir:Path, labels_yaml_path: Path):
 
-    if not (indir / "json_annotations").exists():
-        raise Exception("Directory json_annotations not found in {}".format(indir))
-    if not labels_yaml.exists():
-        raise Exception("Labels yaml file not found in {}".format(labels_yaml))
+    if not indir.exists():
+        raise Exception(
+            "Input dir with json_annotations not found in {}".format(indir.resolve())
+        )
 
-    json_dir = indir / "json_annotations"
+    if not labels_yaml_path.exists():
+        raise Exception("Labels yaml file not found at {}".format(labels_yaml_path))
+    else:
+        labels_info_reader = YamlSegMapReader(labels_yaml_path)
+        labels_parser = SegmentationLabelParser(labels_info_reader)
+
+    json_dir = indir
     json_files = natsorted(json_dir.glob("*.json"))
-
-    labels_info_reader = YamlSegMapReader(labels_yaml)
-    labels_parser = SegmentationLabelParser(labels_info_reader)
 
     for json_file in tqdm(json_files):
         file_prefix = json_file.with_suffix("").name
-        labelme_saver = SaveInSingleFolder(labels_parser, indir, file_prefix)
-        labelme_json = LabelMeJsonParser(json_file, labels_parser, labelme_saver)
+        labelme_saver = MultiDirSaver(labels_parser, outdir, file_prefix)
+        labelme_json = LabelMeJsonParser(json_file, labelme_saver, labels_parser)
         labelme_json.save()
 
 
 @click.command()
 @click.argument("json_file", type=Path)
-@click.argument("out_dir", type=Path)
+@click.option("--outdir", type=Path, required=True)
 @click.option(
-    "--labels_yaml", type=Path, default=None, help="Path to yaml file with class names and ids"
+    "--labels_yaml_path",
+    type=Path,
+    default=None,
+    help="Path to yaml file with class names and ids",
 )
-def parse_single_json_file(json_file: Path, out_dir: Path, labels_yaml: Path):
+def parse_single_file(json_file: Path, outdir: Path, labels_yaml_path: Path):
     """Parse a single labelme json file and extract raw image and label"""
 
-    if not labels_yaml.exists():
-        raise Exception("Labels yaml file not found at {}".format(labels_yaml))
-    if labels_yaml is None:
-        folder_name = json_file.with_suffix("").name
-        # saver = SaveInMultipleFolders(parser, out_dir/folder_name)
-        parser = LabelMeJsonParser(json_file, None, None)
-        # parser.save()
-        parser.save_images_and_labels(out_dir / folder_name)
+    folder_name = json_file.with_suffix("").name
+
+    if labels_yaml_path is None:
+        labelme_parser = LabelMeJsonParser(json_file, None, None)
+        labelme_parser.save_without_label_parser(outdir / folder_name)
     else:
+        if not labels_yaml_path.exists():
+            raise Exception("Labels yaml file not found at {}".format(labels_yaml_path))
 
-        label_info_reader = YamlSegMapReader(labels_yaml)
+        label_info_reader = YamlSegMapReader(labels_yaml_path)
         label_parser = SegmentationLabelParser(label_info_reader)
-
-        folder_name = json_file.with_suffix("").name
-        labelme_saver = SaveInMultipleFolders(label_parser, out_dir / folder_name)
-        labelme_json = LabelMeJsonParser(json_file, label_parser, labelme_saver)
-        labelme_json.save()
+        labelme_saver = SingleDirSaver(label_parser, outdir / folder_name)
+        labelme_parser = LabelMeJsonParser(json_file, labelme_saver, label_parser)
+        labelme_parser.save()
 
 
 if __name__ == "__main__":
-    mycommands.add_command(parse_folder_with_json_files)
-    mycommands.add_command(parse_single_json_file)
+    mycommands.add_command(parse_folder)
+    mycommands.add_command(parse_single_file)
 
     mycommands()
